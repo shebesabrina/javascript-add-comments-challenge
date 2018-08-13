aabc03174155e31ccf394bde9cab509b71fa7a68
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-12 20:12:51 -0600

    comments completed

--- source/addComment.js
+++ source/addComment.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
-  // add your code here
+  $("form").hide();
+
+  $('#new_comment_button').on("click", function() {
+    $("#new_comment_button").hide();
+    $("form").show();
+  });
+
+  $('#form_submit').on("click", function(event) {
+    event.preventDefault();
+    $("form").hide();
+    $("#new_comment_button").show();
+    let comment = $('input[name="comment"]').val();
+    let author = $('input[name="author"]').val();
+    $('input[name="comment"').val('');
+    $('input[name="author"').val('');
+    if (comment) {
+      $(`<li>${comment}<span class="author">${author}</span></li>`).appendTo( "#comment_list" );
+    }
+  });
 })
